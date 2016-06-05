26f86c8762b848372f8cbd4c2cee69c86147ae1f
mohammedshameem <mr.mohammedshameem@gmail.com>
2016-06-05 16:44:53 +0530

    Initial commit

---
+++ app/src/main/java/com/mohammed/shameem/clist/ListData.java
@@ -0,0 +1,37 @@
+package com.mohammed.shameem.clist;
+
+/**
+ * Created by shameem on 6/5/2016.
+ */
+
+public class ListData {
+
+    String Description;
+    String title;
+    int imgResId;
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImgResId() {
+        return imgResId;
+    }
+
+    public void setImgResId(int imgResId) {
+        this.imgResId = imgResId;
+    }
+
+}
